77359a9ee26285ddd2a3cbc4a1b335c239e7027d
AD <eveaterust@gmail.com>
2017-04-24 20:11:36 -0500

    Attempt to at using tkinter to build a ui rebuilding history to remove garbage
    Post launch Django files
    
    Adding Files generated after first app start.
    Added *.pyc to gitignore
    
    Adding core app
    
    Adding core app file
    Configure Django to run core on /core/
    
    Intial Build of sqllite3 Database
    
    Creation of sqllite3 DB
    
    Adding base URL mapping and sub-domains from there
    
    URL available now:
    localhost:8000
    localhost:8000\core
    localhost:8000\core\50 (Any number can be used)
    localhost:8000\core\50\results
    localhost:8000\core\50\vote
    
    Added project to PYTHONPATH to resolve a import issue
    
    View imports were getting flagged in PyDev. Resolved by adding project
    to PYTHONPATH.
    
    Added index.html for base site built out
    
    Added index.html for future work. Added /index/ as url to new view
    index. Index view uses index.html to renmder.
    
    Removing TIME_ZONE causing app launch failures
    
    Removing TIME_ZONE because of error with calling timezone function
    Revisit this in future builds
    
    Added CSS style sheet
    
    Created CSS style shee in static/core
    Loaded CSS into index.html
    
    Restructuring project to better align with requirement
    
    Saving my ass if this goes up in flames
    
    Rebuilt landing page and core page
    
    Restructured project to have landing (home) page and core page
    Removed index and other core subpages (they were for testing)
    Cleaned up model.py
    
    New home page
    
    New home page
    
    removing eclipse files
    
    correct django gitignore
    
    removing sqlite
    
    removing creds even if f ake

---
+++ BeerTapper_Web/core/views.py
@@ -0,0 +1,13 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+
+#def base(request):
+#    return render(request, "home.html")
+
+def core(request):
+    return HttpResponse("Hello, world. APP")
+
+def index(request):
+    return render(request, "index.html")
