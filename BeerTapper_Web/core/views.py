91651fcb5eaa96cec92a4dd5eccfe44ca4ab16e8
AD <eveaterust@gmail.com>
2017-06-04 22:16:55 -0500

    Fixing bad rebase and push and control client caching
    Added cache controlle to view to cause them to refresh browser cacher.
    This fixes a major bug when developing. Rebuilt project from commit
    7b40c51

--- BeerTapper_Web/core/views.py
+++ BeerTapper_Web/core/views.py
@@ -1,12 +1,13 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.views.decorators.cache import never_cache
 
 # Create your views here.
 
-
+@never_cache
 def base(request):
     return render(request, "home.html")
-    
+
 def core(request):
     return HttpResponse("Hello, world. APP")
 
