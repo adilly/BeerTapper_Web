76f9fa768580873dc57572c00aadffeac88cdaa9
AD <eveaterust@gmail.com>
2017-06-10 11:38:20 -0500

    Fixed issue with page view caching. This appears to really fix the issue that @never_cache was suppose to do.

--- BeerTapper_Web/core/views.py
+++ BeerTapper_Web/core/views.py
@@ -1,10 +1,10 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from django.views.decorators.cache import never_cache
+from django.views.decorators.cache import cache_control
 
 # Create your views here.
 
-@never_cache
+@cache_control(max_age=0, no_cache=True, no_store=True, must_revalidate=True)
 def base(request):
     return render(request, "home.html")
 
