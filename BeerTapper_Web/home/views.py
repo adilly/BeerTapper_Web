729457cde9e0b6a54076855728c8a21ea69b94bf
AD <eveaterust@gmail.com>
2017-06-13 21:54:44 -0500

    Still trying to cleanup bad rebase issues... Maybe I'll get this figured out...

--- BeerTapper_Web/home/views.py
+++ BeerTapper_Web/home/views.py
@@ -6,10 +6,4 @@ from django.views.decorators.cache import cache_control
 
 @cache_control(max_age=0, no_cache=True, no_store=True, must_revalidate=True)
 def home(request):
-<<<<<<< HEAD
     return render(request, "home.html")
-=======
-    '''Returns Basic HTML Webpage. This is my playground'''
-    return render(request, "home.html")
-# Create your views here.
->>>>>>> b2f138e... Added doctring to home view
